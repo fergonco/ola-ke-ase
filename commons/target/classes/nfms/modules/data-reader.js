0ecfcfe94d576580a78c56f2cd802ceeb64014a1
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-01-08 07:31:06 +0100

    removed target folder

--- commons/target/classes/nfms/modules/data-reader.js
+++
@@ -1,8 +0,0 @@
-define([ "text!../plan", "utils", "message-bus" ], function(plan, utils, bus) {
-	var plan = JSON.parse(plan);
-
-	bus.listen("modules-loaded", function() {
-		bus.send("plan", [ plan ]);
-	});
-
-});
