76a374455fdb79c410df77054512b0e6c3b04428
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-01-13 11:35:10 +0100

    Edit time records

--- gantt/src/main/resources/nfms/modules/edit-task-names.js
+++ gantt/src/main/resources/nfms/modules/edit-task-names.js
@@ -1,5 +1,5 @@
-define([ "message-bus", "task-tree", "ui-generator", "text!task-schema.json" ], function(bus,
-		taskTree, uiGenerator, taskSchema) {
+define([ "message-bus", "task-tree", "ui-generator", "utils", "text!task-schema.json" ], function(
+		bus, taskTree, uiGenerator, utils, taskSchema) {
 
 	var selectedTaskName;
 
@@ -23,6 +23,7 @@ define([ "message-bus", "task-tree", "ui-generator", "text!task-schema.json" ], 
 			}
 			task["dailyDuration"] = object["dailyDuration"];
 			task["content"] = object["content"];
+			task["timeRecords"] = object["timeRecords"];
 			return true;
 		});
 		generated.formClosedOk(function() {
@@ -42,4 +43,66 @@ define([ "message-bus", "task-tree", "ui-generator", "text!task-schema.json" ], 
 		selectedTaskName = newSelectedName;
 	});
 
+	uiGenerator.setCustomParser("time-records", function(text) {
+		var timeRecords = [];
+		var lines = text.split(/\n/);
+		for (var i = 0; i < lines.length; i++) {
+			if (lines[i].trim().length == 0) {
+				continue;
+			}
+			var parts = lines[i].match(/(.*):(.*)h(.*)m(.*)s.*-(.*)/);
+			var start = new Date(parts[1]);
+			start.setHours(parts[2]);
+			start.setMinutes(parts[3]);
+			start.setSeconds(parts[4]);
+
+			var tr = {
+				"start" : start.getTime()
+			}
+			timeRecords.push(tr);
+			if (parts[5].trim().length > 0) {
+				var totalSeconds = 0;
+				var strTime = parts[5];
+				var currentToken = "";
+				for (var j = 0; j < strTime.length; j++) {
+					var charAt = strTime.charAt(j);
+					if (charAt == "h") {
+						totalSeconds += 60 * 60 * parseInt(currentToken);
+						currentToken = "";
+					} else if (charAt == "m") {
+						totalSeconds += 60 * parseInt(currentToken);
+						currentToken = "";
+					} else if (charAt == "s") {
+						totalSeconds += parseInt(currentToken);
+						currentToken = "";
+					} else {
+						currentToken += charAt;
+					}
+				}
+				if (totalSeconds > 0) {
+					tr["end"] = start.getTime() + 1000 * totalSeconds;
+				}
+			}
+		}
+
+		return timeRecords;
+	});
+	uiGenerator.setCustomRender("time-records", function(value) {
+		var ret = "";
+		if (value) {
+			for (var i = 0; i < value.length; i++) {
+				var tr = value[i];
+
+				var startDate = new Date(tr.start);
+				ret += (startDate.getUTCMonth() + 1) + "/" + startDate.getUTCDate() + "/"
+						+ startDate.getUTCFullYear() + ": ";
+				ret += startDate.getHours() + "h" + startDate.getMinutes() + "m"
+						+ startDate.getSeconds() + "s";
+				ret += " - " + utils.formatTime(tr.end - tr.start);
+
+				ret += "\n";
+			}
+		}
+		return ret;
+	});
 });
