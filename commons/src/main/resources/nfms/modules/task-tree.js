e3c6d9968bd88aa6a31b8b0cf567b25d5a327436
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-02-11 11:16:28 +0100

    Show root load. Individual group limits

--- commons/src/main/resources/nfms/modules/task-tree.js
+++ commons/src/main/resources/nfms/modules/task-tree.js
@@ -15,7 +15,8 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 
 	var ROOT = {
 		"taskName" : "root",
-		"tasks" : null
+		"tasks" : null,
+		"dedicationUpperLimit" : 10
 	}
 
 	var FILTER_ALL = function(task) {
@@ -149,6 +150,17 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 		task.tasks.splice(0, 0, newTask);
 		return newTask.taskName;
 	}
+	function getter(task, propertyName, defaultValue) {
+		if (task.hasOwnProperty(propertyName)) {
+			return task[propertyName];
+		} else {
+			return defaultValue;
+		}
+	}
+	function setter(task, propertyName, value) {
+		task[propertyName] = value;
+		bus.send("dirty");
+	}
 	function decorateTask(parent, task) {
 		task["getParent"] = function() {
 			return parent;
@@ -160,8 +172,7 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 			return task.hasOwnProperty("folded") && task.folded == true;
 		}
 		task["setFolded"] = function(foldedStatus) {
-			task.folded = foldedStatus;
-			bus.send("dirty");
+			setter(task, "folded", foldedStatus);
 		}
 		task["getStatus"] = function() {
 			if (task.isGroup()) {
@@ -358,7 +369,6 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 				var numDays = 0;
 				var day = task.getStartDate().getTime();
 				while (day < task.getEndDate().getTime()) {
-					console.log(new Date(day).toGMTString());
 					if (new Date(day).getUTCDay() > 0 && new Date(day).getUTCDay() < 6) {
 						numDays++;
 					}
@@ -383,8 +393,7 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 			return task.isGroup() ? 0 : 5;
 		}
 		task["setDailyDuration"] = function(newDailyDuration) {
-			task["dailyDuration"] = newDailyDuration;
-			bus.send("dirty");
+			setter(task, "dailyDuration", newDailyDuration);
 		}
 		task["setDayFinished"] = function(finished, day) {
 			var dayList;
@@ -413,15 +422,10 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 			return false;
 		}
 		task["getContent"] = function() {
-			if (task.hasOwnProperty("content")) {
-				return task.content;
-			} else {
-				return "";
-			}
+			return getter(task, "content", "");
 		}
 		task["setContent"] = function(newContent) {
-			task["content"] = newContent;
-			bus.send("dirty");
+			setter(task, "content", newContent);
 		}
 		task["getPresentationTimeDomain"] = function() {
 			return task.getTimeDomain(utils.DAY_MILLIS / 3);
@@ -448,15 +452,10 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 			return ret;
 		}
 		task["isArchived"] = function() {
-			if (task.hasOwnProperty("archived")) {
-				return task.archived;
-			} else {
-				return false;
-			}
+			return getter(task, "archived", false);
 		}
 		task["setArchived"] = function(archived) {
-			task["archived"] = archived;
-			bus.send("dirty");
+			setter(task, "archived", archived);
 		}
 		var toHour = function(date) {
 			return date.getUTCHours() + date.getMinutes() / 60;
@@ -474,8 +473,7 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 			}
 		}
 		task["setDayStart"] = function(dayStart) {
-			task.dayStart = dayStart;
-			bus.send("dirty");
+			setter(task, "dayStart", dayStart);
 		}
 		task["getDayEnd"] = function() {
 			if (task.hasOwnProperty("dayEnd")) {
@@ -490,8 +488,7 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 			}
 		}
 		task["setDayEnd"] = function(dayEnd) {
-			task.dayEnd = dayEnd;
-			bus.send("dirty");
+			setter(task, "dayEnd", dayEnd);
 		}
 		task["isPlannedInDay"] = function() {
 			if (task.hasOwnProperty("plannedInDay")) {
@@ -510,19 +507,25 @@ define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 			}
 		}
 		task["setPlannedInDay"] = function(planned) {
-			task.plannedInDay = planned;
-			bus.send("dirty");
+			setter(task, "plannedInDay", planned);
 		}
 		task["getImportance"] = function() {
-			if (task.hasOwnProperty("importance")) {
-				return task.importance;
-			} else {
-				return 0;
-			}
+			return getter(task, "importance", 0);
 		}
 		task["setImportance"] = function(importance) {
-			task.importance = importance;
-			bus.send("dirty");
+			setter(task, "importance", importance);
+		}
+		task["getDedicationLowerLimit"] = function() {
+			return getter(task, "dedicationLowerLimit", null);
+		}
+		task["getDedicationUpperLimit"] = function() {
+			return getter(task, "dedicationUpperLimit", null);
+		}
+		task["setDedicationLowerLimit"] = function(limit) {
+			setter(task, "dedicationLowerLimit", limit);
+		}
+		task["setDedicationUpperLimit"] = function(limit) {
+			setter(task, "dedicationUpperLimit", limit);
 		}
 	}
 
--- gantt/src/main/resources/nfms/modules/show-load.js
+++ gantt/src/main/resources/nfms/modules/show-load.js
@@ -1,5 +1,8 @@
 define([ "utils", "message-bus", "task-tree", "d3" ], function(utils, bus, taskTree) {
 
+	var NONE = 0, ROOT = 1, GROUPS = 2;
+
+	var mode = 0;
 	var timeDomain;
 	var xScale;
 	var yScale;
@@ -10,26 +13,43 @@ define([ "utils", "message-bus", "task-tree", "d3" ], function(utils, bus, taskT
 		var xScale = taskTree.getXScale();
 		var yScale = taskTree.getYScale();
 		var dayWidthPixels = xScale(new Date(timeDomain[0].getTime() + utils.DAY_MILLIS)) - xScale(timeDomain[0]);
-		var colors = [ "f", "f", "f", "f", "f", "a", "a", "a", "a", "7", "7", "7", "3" ];
 		loadSelection.attr("class", "load")//
 		.attr("x", function(d, index) {
-			var task = taskTree.getTask(d.groupName);
+			var task;
+			if (d.groupName == "root") {
+				task = taskTree.ROOT;
+			} else {
+				task = taskTree.getTask(d.groupName);
+			}
 			var taskTimeDomain = task.getTimeDomain(0);
 			return xScale(taskTimeDomain[0].getTime() + d.groupIndex * utils.DAY_MILLIS);
 		})//
 		.attr("y", function(d) {
-			return yScale(d.groupName);
+			if (d.groupName == "root") {
+				return 0;
+			} else {
+				return yScale(d.groupName);
+			}
 		})//
 		.attr("width", dayWidthPixels)//
 		.attr("height", yScale.rangeBand() / 2)//
 		.style("stroke", "black")//
 		.style("fill", function(d) {
-			var load = Math.round(d.load);
-			if (load >= colors.length) {
-				return "red";
+			var task;
+			if (d.groupName == "root") {
+				task = taskTree.ROOT;
 			} else {
-				var color = colors[load];
-				return "#" + color + color + color;
+				task = taskTree.getTask(d.groupName);
+			}
+			var min = task.getDedicationLowerLimit();
+			var max = task.getDedicationUpperLimit();
+
+			if (max != null && d.load > max) {
+				return "red";
+			} else if (min != null && d.load < min) {
+				return "blue";
+			} else {
+				return "white";
 			}
 		})//
 		.attr("title", function(d, index) {
@@ -37,15 +57,20 @@ define([ "utils", "message-bus", "task-tree", "d3" ], function(utils, bus, taskT
 		});
 	}
 
-	bus.listen("gantt-created", function() {
+	function refresh() {
 		var timeDomain = taskTree.getTimeDomain();
-		var groups = taskTree.visitTasksIgnoreUserFilter(taskTree.ROOT, function(task) {
-			return task != taskTree.ROOT && task.isGroup();
-		}, taskTree.VISIT_UNFOLDED_CHILDREN, function(task) {
-			return task;
-		});
+		var groups = null;
+		if (mode == GROUPS) {
+			groups = taskTree.visitTasksIgnoreUserFilter(taskTree.ROOT, function(task) {
+				return task != taskTree.ROOT && task.isGroup();
+			}, taskTree.VISIT_UNFOLDED_CHILDREN, function(task) {
+				return task;
+			});
+		} else if (mode == ROOT) {
+			groups = [ taskTree.ROOT ];
+		}
 		var loadData = [];
-		for (var i = 0; i < groups.length; i++) {
+		for (var i = 0; mode != NONE && i < groups.length; i++) {
 			var groupLoadData = [];
 			var groupTimeDomain = groups[i].getTimeDomain(0);
 			var dayCount = Math.round((groupTimeDomain[1].getTime() - groupTimeDomain[0].getTime()) / utils.DAY_MILLIS);
@@ -57,7 +82,7 @@ define([ "utils", "message-bus", "task-tree", "d3" ], function(utils, bus, taskT
 				});
 			}
 			var tasks = taskTree.visitTasksIgnoreUserFilter(groups[i], function(task) {
-				return !task.isGroup();
+				return !task.isGroup() && !task.isArchived();
 			}, taskTree.VISIT_ALL_CHILDREN, function(task) {
 				return task;
 			});
@@ -78,6 +103,15 @@ define([ "utils", "message-bus", "task-tree", "d3" ], function(utils, bus, taskT
 		loadSelection.exit().remove();
 		loadSelection.enter().append("rect");
 		updateLoadSelection(loadSelection);
+	}
+
+	bus.listen("gantt-created", refresh);
+
+	d3.select("body").append("div")//
+	.attr("class", "button")//
+	.html("Mostrar dedicación grupos").on("click", function() {
+		mode = (mode + 1) % 3;
+		refresh();
 	});
 
 	bus.listen("selection-update", function() {
